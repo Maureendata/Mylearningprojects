950f53599618d4a56d056c2d35a48978d21921ad
maureen <maureenotieno38@gmail.com>
2025-01-28 12:42:23 +0300

    Code commit

--- Mylearningprojects/src/Components/Code.jsx
+++ Mylearningprojects/src/Components/Code.jsx
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
 export default function Code() {
-    return( <div>
-        Coding is a fun thing to do everyday.
-    </div>);
+  return <div>Coding is a fun thing to do everyday.</div>;
 }
-=======
-export default function Code(){
-    return <div>Coding is fun</div>;
-}
->>>>>>> 07140931c97b8ebf92e3b9d0d4bb27fa506c240e
